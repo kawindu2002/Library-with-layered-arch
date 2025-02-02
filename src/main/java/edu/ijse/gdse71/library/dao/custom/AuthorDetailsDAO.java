3b743017d3839aff1c93f86dda6157041f3052e1
kawindu2002 <kawindupriyashan@gmail.com>
2025-02-02 21:50:46 +0530

    change audetailsdao

--- src/main/java/edu/ijse/gdse71/library/dao/custom/AuthorDetailsDAO.java
+++ src/main/java/edu/ijse/gdse71/library/dao/custom/AuthorDetailsDAO.java
@@ -1,11 +1,12 @@
 package edu.ijse.gdse71.library.dao.custom;
 
+import edu.ijse.gdse71.library.dao.SuperDAO;
 import edu.ijse.gdse71.library.dto.AuthorDetailsDTO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface AuthorDetailsDAO {
+public interface AuthorDetailsDAO extends SuperDAO {
 
      boolean saveAuthorDetailsList(ArrayList<AuthorDetailsDTO> authorDetailsDTOS) throws SQLException;
 
