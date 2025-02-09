cda386217094f668f15846918618dccd19c29b74
kawindu2002 <kawindupriyashan@gmail.com>
2025-02-10 00:17:40 +0530

    update method

--- src/main/java/edu/ijse/gdse71/library/dao/custom/UserDAO.java
+++ src/main/java/edu/ijse/gdse71/library/dao/custom/UserDAO.java
@@ -1,13 +1,9 @@
 package edu.ijse.gdse71.library.dao.custom;
 
 import edu.ijse.gdse71.library.dao.CrudDAO;
-import edu.ijse.gdse71.library.dto.UserDTO;
 import edu.ijse.gdse71.library.entity.User;
-import edu.ijse.gdse71.library.util.CrudUtil;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public interface UserDAO extends CrudDAO<User> {
     String getState(String userId) throws SQLException;
